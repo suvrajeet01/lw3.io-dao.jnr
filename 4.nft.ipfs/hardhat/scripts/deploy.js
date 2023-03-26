14d21dca45faba85e6fa19c1487d4593e79fad89
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2023-03-27 02:49:03 +0530

    nft.ipfs

---
+++ 4.nft.ipfs/hardhat/scripts/deploy.js
@@ -0,0 +1,145 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://QmPJkQ8VX1HqbHzHB3JPs47yE7BVD8N3B3GjbFcRwhkN4t";
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
+  */
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  // deploy the contract
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // We require the Hardhat Runtime Environment explicitly here. This is optional
+// // but useful for running the script in a standalone fashion through `node <script>`.
+// //
+// // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// // will compile your contracts, add the Hardhat Runtime Environment's members to the
+// // global scope, and execute the script.
+// const hre = require("hardhat");
+
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
+
+//   const lockedAmount = hre.ethers.utils.parseEther("0.001");
+
+//   const Lock = await hre.ethers.getContractFactory("Lock");
+//   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+//   await lock.deployed();
+
+//   console.log(
+//     `Lock with ${ethers.utils.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+//   );
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
